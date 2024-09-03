029db3a95cc5143867f2f83ecb0b679d035c8867
reem <reem.alon@lilibanking.com>
2024-09-03 14:30:32 +0300

    *WIP*

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,35 +1,135 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import { formOptions, useForm } from '@tanstack/react-form';
+import { zodValidator } from '@tanstack/zod-form-adapter';
+import { z } from 'zod';
+import { useEffect } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { FloatLabel } from 'primereact/floatlabel';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+/** Form Data Interface */
+interface User {
+  name: string;
+  age: string;
+  color: string;
 }
 
-export default App
+const mockGlobalState: { userData: User } = {
+  userData: {
+    name: 'Reem',
+    age: '29',
+    color: 'Blue',
+  },
+};
+
+function App() {
+  /** Form Options */
+  const formOpts = formOptions<User>({
+    /** Default values in case you have prefill data */
+    defaultValues: {
+      name: mockGlobalState.userData.name,
+      age: mockGlobalState.userData.age,
+      color: mockGlobalState.userData.color,
+    },
+  });
+
+  /** Form Instance Creation */
+  const form = useForm({
+    ...formOpts,
+    onSubmit: ({ value }) => {
+      console.log(value);
+    },
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      /** Manual form validation on mount */
+      // form.handleSubmit();
+    }, 3000);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center py-[50px] w-full h-full">
+      <h1 className="mb-[28px]">Form example</h1>
+      <form
+        className="flex flex-col gap-[14px] items-center w-[600px]"
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        {/** Controlled Form Field */}
+        <form.Field
+          name="name"
+          /** Using Validation Library - Zod */
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChange: z
+              .string()
+              .min(3, 'min 3 chars')
+              .max(10, 'max 10 characters'),
+          }}
+          children={(field) => {
+            return (
+              <>
+                <input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  onFocus={() => field.setErrorMap({})}
+                />
+                {/** Show field error value from local error map */}
+                <em role="alert">{field.state.meta.errors.join(', ')}</em>
+              </>
+            );
+          }}
+        />
+        <form.Field
+          name="age"
+          /** Using Validation Library - Zod */
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChange: z
+              .string()
+              .min(3, 'min 3 chars')
+              .max(10, 'max 10 characters'),
+          }}
+          children={(field) => {
+            return (
+              <>
+                <FloatLabel>
+                  <InputText
+                    className="text-black"
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    onFocus={() => field.setErrorMap({})}
+                  />
+                  <label htmlFor={field.name}>{field.name}</label>
+                </FloatLabel>
+                {/** Show field error value from local error map */}
+                <em role="alert">{field.state.meta.errors.join(', ')}</em>
+              </>
+            );
+          }}
+        />
+        <form.Subscribe
+          selector={(state) => [state.canSubmit]}
+          children={([canSubmit]) => (
+            <button type="submit">{canSubmit ? 'Submit' : 'Fix Form'}</button>
+          )}
+        />
+
+        {/** Always enabled button */}
+        {/*<button type="submit">Submit</button>*/}
+      </form>
+    </div>
+  );
+}
+
+export default App;
